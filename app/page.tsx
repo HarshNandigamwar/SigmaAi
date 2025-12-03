5ab7eb74b0620e2b1d9b85b9a22bc85465381686
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2025-12-03 17:23:50 +0530

    Add console Signature

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+import { useEffect } from "react";
 import HomePage from "./home/page";
+import { printConsoleSignature } from "./util/printConsoleSignature";
 export default function Home() {
+  useEffect(() => {
+    printConsoleSignature();
+  }, []);
   return (
     <main className="h-screen">
       <HomePage />
