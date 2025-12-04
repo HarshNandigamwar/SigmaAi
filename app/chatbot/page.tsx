6d67256782c4ee17a4f40c055d343744e4552621
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2025-12-04 19:43:56 +0530

    loader done

--- app/chatbot/page.tsx
+++ app/chatbot/page.tsx
@@ -144,7 +144,7 @@ const ChatWindow = () => {
         ))}
 
         {/* Display Loader for Response */}
-        {!isAILoading && <Loader />}
+        {isAILoading && <Loader />}
         <div ref={messagesEndRef} />
       </div>
 
